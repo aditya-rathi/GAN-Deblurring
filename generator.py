78fc570e44cd8640381096b0d87a89f11d6c7b64
vatsaljoshi00 <93884491+vatsaljoshi00@users.noreply.github.com>
2021-11-13 18:13:04 -0500

    Add files via upload

--- generator.py
+++ generator.py
@@ -1,29 +1,56 @@
-import keras
-from keras.layers import Input, Conv2D, Activation, BatchNormalization
-from keras.layers.merge import Add
-from keras.layers.core import Dropout
 from keras.layers import Input, Activation, Add
+from keras.layers.advanced_activations import LeakyReLU
+from keras.layers.convolutional import Conv2D, Conv2DTranspose
+from keras.layers.core import Lambda
+from keras.layers.normalization import BatchNormalization
+from keras.models import Model
 
 from layer_utils import ReflectionPadding2D, res_block
 
-def res_block(input, filters, kernel_size=(3,3), strides=(1,1), use_dropout=False):
+ngf = 64
+input_nc = 3
+output_nc = 3
+input_shape_generator = (256, 256, input_nc)
+n_blocks_gen = 9
 
-    x = ReflectionPadding2D((1,1))(input)
-    x = Conv2D(filters=filters,
-               kernel_size=kernel_size,
-               strides=strides,)(x)
+
+def generator_model():
+    """Build generator architecture."""
+    # Current version : ResNet block
+    inputs = Input(shape=image_shape)
+
+    x = ReflectionPadding2D((3, 3))(inputs)
+    x = Conv2D(filters=ngf, kernel_size=(7,7), padding='valid')(x)
     x = BatchNormalization()(x)
     x = Activation('relu')(x)
 
-    if use_dropout:
-        x = Dropout(0.5)(x)
+    # Increase filter number
+    n_downsampling = 2
+    for i in range(n_downsampling):
+        mult = 2**i
+        x = Conv2D(filters=ngf*mult*2, kernel_size=(3,3), strides=2, padding='same')(x)
+        x = BatchNormalization()(x)
+        x = Activation('relu')(x)
 
-    x = ReflectionPadding2D((1,1))(x)
-    x = Conv2D(filters=filters,
-                kernel_size=kernel_size,
-                strides=strides,)(x)
-    x = BatchNormalization()(x)
+    # Apply 9 ResNet blocks
+    mult = 2**n_downsampling
+    for i in range(n_blocks_gen):
+        x = res_block(x, ngf*mult, use_dropout=True)
 
-    # Two convolution layers followed by a direct connection between input and output
-    merged = Add()([input, x])
-    return merged
+    # Decrease filter number to 3 (RGB)
+    for i in range(n_downsampling):
+        mult = 2**(n_downsampling - i)
+        x = Conv2DTranspose(filters=int(ngf * mult / 2), kernel_size=(3,3), strides=2, padding='same')(x)
+        x = BatchNormalization()(x)
+        x = Activation('relu')(x)
+
+    x = ReflectionPadding2D((3,3))(x)
+    x = Conv2D(filters=output_nc, kernel_size=(7,7), padding='valid')(x)
+    x = Activation('tanh')(x)
+
+    # Add direct connection from input to output and recenter to [-1, 1]
+    outputs = Add()([x, inputs])
+    outputs = Lambda(lambda z: z/2)(outputs)
+
+    model = Model(inputs
+    return model
